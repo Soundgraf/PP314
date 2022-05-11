ce1cfc2ffc630faa0e682bb1e24fe11886c7bb37
Soundgraf <soundgraf@mail.ru>
2022-05-11 14:21:18 +0300

    Commit02

--- src/main/java/ru/kata/spring/boot_security/demo/controller/MyController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MyController.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,8 @@ public class MyController {
     private final UserService userService;
 
     @GetMapping("/user")
-    public String oneUser(Model model, Principal principal) {
-        model.addAttribute("oneUser", userService.loadUserByUsername(principal.getName()));
+    public String oneUser(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("oneUser", userService.loadUserByUsername(user.getEmail()));
         return "user";
     }
 
